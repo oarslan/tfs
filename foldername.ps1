d671d571dbe13304b092cb2bb163dfcf8585832f
Özlem Arslan <ce.ozlemarslan@gmail.com>
2020-02-18 15:43:10 +0300

    Add files via upload

---
+++ foldername.ps1
@@ -0,0 +1,15 @@
+﻿
+$secpasswd = ConvertTo-SecureString "yourpass" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("youruser", $secpasswd)
+
+$response =  Invoke-RestMethod -Credential $mycreds -Uri "http://yourtfsname/tfs/collectionname/_apis/tfvc/items?scopePath=yourpath"
+
+$result= $response.value  | Sort-Object $response.value.changeDate
+
+foreach ($item in $result) {
+
+   $a= $item.changeDate  + " "  + $item.path
+   
+    Write-Output $a 
+
+}
---
+++ projectlist.ps1
@@ -0,0 +1,9 @@
+﻿
+$secpasswd = ConvertTo-SecureString "yourpass" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("youruser", $secpasswd)
+
+
+$response =  Invoke-RestMethod -Credential $mycreds -Uri "http://yourtfsname/tfs/collectionname/_apis/projects"
+
+$response.value
+
